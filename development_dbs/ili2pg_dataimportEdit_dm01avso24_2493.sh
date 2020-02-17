c45c091ecbac37a922d14d95b3a94eefbcc385f3
bjsvwcur <38523407+bjsvwcur@users.noreply.github.com>
2020-02-17 17:05:04 +0100

    new files

---
+++ development_dbs/ili2pg_dataimportEdit_dm01avso24_2493.sh
@@ -0,0 +1,4 @@
+java -jar /home/bjsvwcur/ilis/ili2pg-4.3.1/ili2pg.jar --import --dbschema agi_dm01avso24 --models DM01AVSO24LV95 \
+--dbhost localhost --dbport 54321 --dbdatabase edit --dbusr admin --dbpwd admin \
+--disableAreaValidation --disableValidation --dataset "2493" \
+agi_dm01avso24_2493.xtf
